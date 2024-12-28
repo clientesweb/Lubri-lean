8e05c34bd37b88d7bb9285459ebec6f9d5cdc2e8
clientesweb <alexisifran32@gmail.com>
2024-12-27 21:36:12 -0300

    Add files via upload

---
+++ lib/constants.ts
@@ -0,0 +1,22 @@
+import type { Brand } from '@/types'
+
+export const BRAND: Brand = {
+  colors: {
+    primary: '#FFD700', // Yellow from logo
+    secondary: '#FF0000', // Red from logo
+    dark: '#000000',
+    light: '#FFFFFF',
+  },
+  social: {
+    instagram: 'https://www.instagram.com/lubrilean.pdas',
+    whatsapp: 'https://wa.me/5493765214214',
+    facebook: 'https://facebook.com/lubrilean',
+  },
+  contact: {
+    phone: '+54 9 376 521-4214',
+    email: 'contacto@lubrilean.com',
+    address: 'Posadas, Misiones, 3300',
+    maps: 'https://maps.app.goo.gl/5kvnS2CvodN1LQDE7'
+  }
+}
+
